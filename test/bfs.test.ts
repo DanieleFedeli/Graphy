ec0c59c30472ea413bc95abab70bb535a9c15eef
Daniele Fedeli <d.fedeli@commongrounds.it>
2022-01-08 14:25:27 +0100

    Added 100% cov

--- test/bfs.test.ts
+++ test/bfs.test.ts
@@ -3,19 +3,28 @@ import { GraphNode } from "../src/model/node";
 import { BFS } from "../src/utils/bfs";
 import { buildGraph } from "./fixture/graph.build";
 
-tap.test("BFS testing the right order", async (t) => {
-  t.plan(1);
+tap.test("BFS testing the right order", async t => {
+	t.plan(1);
 
-  const graph = buildGraph();
+	const graph = buildGraph();
 
-  const visited: number[] = [];
-  const correctVisited = [0, 1, 2, 3, 4, 5];
-  const cb = (_err: any, node: GraphNode) => {
-    visited.push(node.data as number);
-  };
+	const visited: number[] = [];
+	const correctVisited = [0, 1, 2, 3, 4, 5];
+	const cb = (_err: any, node: GraphNode) => {
+		visited.push(node.data as number);
+	};
 
-  const node = [...graph.getNodes().values()][0];
-  await BFS(node, cb);
+	const node = [...graph.getNodes().values()][0];
+	await BFS(node, cb);
 
-  t.same(visited, correctVisited);
+	t.same(visited, correctVisited);
+});
+
+tap.test("BFS With no callback", async t => {
+	t.plan(1);
+
+	const graph = buildGraph();
+
+	const node = [...graph.getNodes().values()][0];
+	t.resolves(BFS(node));
 });
--- test/dfs.test.ts
+++ test/dfs.test.ts
@@ -3,19 +3,28 @@ import { GraphNode } from "../src/model/node";
 import { DFS } from "../src/utils/dfs";
 import { buildGraph } from "./fixture/graph.build";
 
-tap.only("Right order", async (t) => {
-  t.plan(1);
-  const graph = buildGraph();
+tap.only("Right order", async t => {
+	t.plan(1);
+	const graph = buildGraph();
 
-  const correctVisited = [0, 2, 5, 1, 4, 3];
-  const visited: number[] = [];
-  const cb = (_err: any, node: GraphNode) => {
-    visited.push(node.data as number);
-  };
+	const correctVisited = [0, 2, 5, 1, 4, 3];
+	const visited: number[] = [];
+	const cb = (_err: any, node: GraphNode) => {
+		visited.push(node.data as number);
+	};
 
-  const node = [...graph.getNodes().values()][0];
+	const node = [...graph.getNodes().values()][0];
 
-  await DFS(node, cb);
+	await DFS(node, cb);
 
-  t.same(visited, correctVisited);
+	t.same(visited, correctVisited);
+});
+
+tap.test("DFS With no callback", async t => {
+	t.plan(1);
+
+	const graph = buildGraph();
+
+	const node = [...graph.getNodes().values()][0];
+	t.resolves(DFS(node));
 });
